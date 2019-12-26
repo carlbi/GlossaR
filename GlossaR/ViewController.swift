8a2038ad34325476deeb3795b94a66a859fd90f5
Carl Biagosch <carl.biagosch@gmail.com>
2019-12-26 19:27:07 +0100

    Working UI

---
+++ GlossaR/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GlossaR
+//
+//  Created by Carl Philipp Biagosch on 26.12.19.
+//  Copyright © 2019 Project GlossaR. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
