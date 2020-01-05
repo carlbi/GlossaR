9826e84d44a7f5571457d63f18342fa4538d2cc4
Carl Biagosch <carl.biagosch@gmail.com>
2020-01-05 23:40:22 +0100

    Basic functionality

--- GlossaR/ViewController.swift
+++ GlossaR/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import CoreData
 
 class ViewController: NSViewController, NSTextFieldDelegate {
     
@@ -15,14 +16,35 @@ class ViewController: NSViewController, NSTextFieldDelegate {
     @IBOutlet weak var TextEnter: NSTextField!
     @IBOutlet weak var KeywordsEnter: NSTextField!
     @IBOutlet weak var ArticleTitle: NSTextField!
+    @IBOutlet weak var GlossaryList: NSTableView!
     
-
+    @IBOutlet weak var tableView: NSTableView!
+    
+    var entries: [NSManagedObject]? = []
+    
+    var container: NSPersistentContainer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Fetching from Core Data
+        self.updateEntries()
         // Delegates
         self.ArticleTitle.delegate = self
         self.TextEnter.delegate = self
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        // Show "nothing selected"
+        self.nothingSelected()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    func nothingSelected() {
         // Disable TextEnter until selected
         self.TextEnter.isEditable = false
         self.TextEnter.stringValue = "Nothing selected"
@@ -33,14 +55,8 @@ class ViewController: NSViewController, NSTextFieldDelegate {
         self.KeywordsEnter.alignment = .center
         self.KeywordsEnter.textColor = NSColor.lightGray
         // Disable ArticleTitle until selected
+        self.ArticleTitle.stringValue = "Please select article"
         self.ArticleTitle.isEditable = false
-
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
     
     @IBAction func AddButtonPressed(_ sender: NSButton) {
@@ -59,6 +75,8 @@ class ViewController: NSViewController, NSTextFieldDelegate {
         self.KeywordsEnter.textColor = NSColor.black    }
     
     @IBAction func DeleteButtonPressed(_ sender: NSButton) {
+        self.deleteEntry()
+        self.reloadFileList()
     }
     
     @IBAction func textFieldAction(sender: NSTextField) {
@@ -72,9 +90,143 @@ class ViewController: NSViewController, NSTextFieldDelegate {
             }
             else if(self.KeywordsEnter == sender){
                 // Save and add to tableview
+                self.saveEntry()
+                self.reloadFileList()
             }
         }
     }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        self.displayEntry()
+    }
+    
+    func displayEntry() {
+        let entrySelected = tableView.selectedRow
+        if entrySelected < 0 {
+            self.nothingSelected()
+            return
+        }
+        guard let entry = entries?[entrySelected] else {
+            return
+        }
+        let name: String = entry.value(forKey: "name") as! String
+        let text: String = entry.value(forKey: "text") as! String
+        let keywords: String = entry.value(forKey: "keywords") as! String
+        
+        self.ArticleTitle.stringValue = name
+        self.TextEnter.stringValue = text
+        self.KeywordsEnter.stringValue = keywords
+    }
+    
+    func reloadFileList() {
+        self.updateEntries()
+        tableView.reloadData()
+    }
+    
+    func saveEntry() {
+        
+        let name: String = self.ArticleTitle.stringValue
+        let text: String = self.TextEnter.stringValue
+        let keywords: String = self.KeywordsEnter.stringValue
+        
+        guard let appDelegate = NSApp.delegate as? AppDelegate else {
+                return
+        }
+
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Entry",
+                                       in: managedContext)!
+        let entry = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        entry.setValue(name, forKeyPath: "name")
+        entry.setValue(text, forKeyPath: "text")
+        entry.setValue(keywords, forKeyPath: "keywords")
+
+        do {
+            try managedContext.save()
+            self.updateEntries()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func deleteEntry(){
+        let name: String = self.ArticleTitle.stringValue
+        
+        guard let appDelegate = NSApp.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Entry")
+        guard let result = try? managedContext.fetch(fetchRequest) else { return }
+        for object in result {
+            if object.value(forKey: "name") as! String == name {
+                managedContext.delete(object)
+            }
+        }
+        do {
+            try managedContext.save()
+            self.updateEntries()
+            self.nothingSelected()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func updateEntries() {
+        guard let appDelegate =
+            NSApp.delegate as? AppDelegate else {
+                return
+        }
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Entry")
+        do {
+            entries = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+
 
 }
 
+extension ViewController: NSTableViewDataSource {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return entries?.count ?? 0
+    }
+    
+}
+
+extension ViewController: NSTableViewDelegate {
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+
+        var text: String = ""
+        var cellIdentifier: String = ""
+        
+        guard let item = entries?[row] else {
+            return nil
+        }
+        
+        if tableColumn == tableView.tableColumns[0] {
+            text = item.value(forKey: "name") as! String
+            cellIdentifier = "EntryCell"
+        }
+        
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            return cell
+        }
+        return nil
+    }
+    
+}
